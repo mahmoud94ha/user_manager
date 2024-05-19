ef1274102d0dddebf0a6c1db1ac362184307e8aa
mahmoud94ha <mahmoudhannani3@gmail.com>
2024-05-19 04:42:57 +0100

    delayed return

--- lib/useClientIP.ts
+++ lib/useClientIP.ts
@@ -4,23 +4,20 @@ import axios from 'axios';
 export default function useClientIP() {
     const [clientIP, setClientIP] = useState(null);
 
-    useEffect(() => {
-        const fetchIP = async () => {
-            try {
-                const headers = {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
-                };
-                const response = await axios.get('https://httpbin.org/ip', { headers });
-                const ip = response.data.origin;
-                setClientIP(ip);
-            } catch (error) {
-                console.error('Error fetching IP:', error);
-                setClientIP(null);
-            }
-        };
-
-        fetchIP();
-    }, []);
-
+    const fetchIP = async () => {
+        try {
+            const headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+            };
+            const response = await axios.get('https://httpbin.org/ip', { headers });
+            const ip = response.data.origin;
+            console.log(ip);
+            setClientIP(ip);
+        } catch (error) {
+            console.error('Error fetching IP:', error);
+            setClientIP(null);
+        }
+    };
+    fetchIP();
     return btoa(clientIP);
 }
