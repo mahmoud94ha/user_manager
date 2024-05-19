01786d4e2ff1872d8399e80364e5ab2a6437178c
mahmoud94ha <mahmoudhannani3@gmail.com>
2024-05-19 04:56:46 +0100

    test

--- lib/useClientIP.ts
+++ lib/useClientIP.ts
@@ -1,22 +1,25 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useClientIP() {
-    const [clientIP, setClientIP] = useState(null);
+  const [clientIP, setClientIP] = useState(null);
 
-    useEffect(() => {
-        const fetchIP = async () => {
-            try {
-                const response = await axios.get('https://httpbin.org/ip');
-                setClientIP(response.data.origin);
-            } catch (error) {
-                console.error('Error fetching IP:', error);
-                setClientIP(null);
-            }
-        };
+  useEffect(() => {
+    const fetchIP = async () => {
+      try {
+        const response = await axios.get("https://httpbin.org/ip");
+        setClientIP(response.data.origin);
+      } catch (error) {
+        console.error("Error fetching IP:", error);
+        setClientIP(null);
+      }
+    };
 
-        fetchIP();
-    }, []);
+    fetchIP();
+  }, []);
 
+  if (clientIP) {
     return btoa(clientIP);
+  }
+  return null;
 }
